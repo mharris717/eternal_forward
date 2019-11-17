c9405e6b4fc312b77a4e2a67d9a021591f24f231
Mike Harris <mharris717@gmail.com>
2019-11-17 10:46:15 -0500

    Initial State

---
+++ lib/eternal_forward.rb
@@ -0,0 +1,6 @@
+require "eternal_forward/version"
+
+module EternalForward
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/eternal_forward/version.rb
@@ -0,0 +1,3 @@
+module EternalForward
+  VERSION = "0.1.0"
+end
